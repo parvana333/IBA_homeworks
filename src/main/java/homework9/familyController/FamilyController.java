257c6a495e72097a3a51fa356914ec85bf52a60a
parvana33 <pervane.yunusova638@gmail.com>
2019-11-05 21:43:59 +0400

    homeworks

--- src/main/java/homework9/familyController/FamilyController.java
+++ src/main/java/homework9/familyController/FamilyController.java
@@ -2,8 +2,17 @@ package homework9.familyController;
 
 import homework9.familyService.FamilyService;
 
+import java.util.List;
+
 public class FamilyController {
     private FamilyService familyService;
+
+
+    //should i follow this way for each method?
+
+    public List<Family> getAllFamilies(){
+        return familyService.getAllFamilies();
+    }
 
 
 
